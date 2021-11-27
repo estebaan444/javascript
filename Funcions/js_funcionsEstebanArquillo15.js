1ece6c5ceb1dff8338b3a9d52ea42aa57e36bce5
estebaan444 <94048166+estebaan444@users.noreply.github.com>
2021-11-27 13:52:36 +0100

    Delete js_funcionsEstebanArquillo15.js

--- Funcions/js_funcionsEstebanArquillo15.js
+++
@@ -1,9 +0,0 @@
-var pos = (a, b) => {
-    return (a.length - a.indexOf(b));
-}
-console.log(posicio('Es divendres', 'que tal'));
-
-var dividir = (a, b) => {
-    return b.substr(a, b.length);
-}
-console.log(dividir(posicio('Es divendres', 'que tal'), 'Hey'));
