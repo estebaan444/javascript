43ebaffb9cc731bf8447f9517b6b47e534df87aa
estebaan444 <esteban.arquillo@institutvidreres.cat>
2021-11-29 08:30:40 +0100

    ex1 lògica

---
+++ Lògica/js_logicaEstebanArquillo1.js
@@ -0,0 +1,14 @@
+var numero = 40;
+
+if((numero % 7) == 0){
+    console.log('El numero es multiple de 7');
+}
+
+else if((numero % 3) == 0){
+    console.log('El numero es multiple de 3');
+}
+
+else{
+    console.log('Este numero no es divisible ni de 3 ni de 7');
+}
+
