61ef23cdf7e1592f7319187554ad0c7c250eacbf
estebaan444 <esteban.arquillo@institutvidreres.cat>
2021-11-29 09:00:55 +0100

    ex2 + correcció 1

---
+++ Lògica/js_logicaEstebanArquillo2.js
@@ -0,0 +1,10 @@
+var x = 15;
+var y = 4;
+
+rang = x > 10 && x <= 50
+    ? console.log(x, "Dins del rang")
+    : console.log(x, 'Fora del rang')
+
+rang = y > 10 && x <= 50
+    ? console.log(y, " Dins del rang")
+    : console.log(y, " Fora del rang")
